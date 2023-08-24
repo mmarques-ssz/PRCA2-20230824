9a7cc1a0e08820c542f62a62b96f3d3b553513e1
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-08-24 20:33:02 -0300

    Add files via upload

---
+++ projPOO1/projPOO1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            inverteTela();
+            Soma s;
+            //s = new Soma();
+            s = new Soma(7,8);
+
+            s.setN1(5);
+            s.setN2(6);
+            //s.calcular();
+            Console.WriteLine("A soma de {0} com {1} é {2}", 
+                s.getN1(), s.getN2(), s.getResultado());
+        }
+
+        static void inverteTela()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+        }
+    }
+}
